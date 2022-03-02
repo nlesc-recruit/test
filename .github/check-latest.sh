b954035dbd1c37540b3b736eec8645a25ea452b5
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2022-03-02 14:39:07 +0100

    now checking latest version in a shell script

---
+++ .github/check-latest.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+LATEST=$(curl --header "Accept: application/vnd.github.v3+json" https://api.github.com/repos/nlesc-recruit/cudawrappers/releases/latest | jq .tag_name --raw-output)
+cd external/cudawrappers
+MY_TAGS=$(git tag --points-at HEAD)
+echo $MY_TAGS
+for tag in $MY_TAGS
+do
+    echo "Checking tag $tag..."
+    if [ "$tag" = "$LATEST" ]; then
+        echo "It's all good., this repo's submodule is using cudawrappers latest tag $tag."
+        exit 0
+    fi
+done
+echo "This repo's submodule is using an older version of the cudawrappers library."
+exit 1
