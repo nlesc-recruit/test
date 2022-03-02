9404c04bd44eb11af68222e1c8dc44f8f0e2c1d8
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2022-03-02 15:26:27 +0100

    tryin g a differnt approach for comparing the submodule version

--- .github/check-latest.sh
+++ .github/check-latest.sh
@@ -1,16 +1,13 @@
 #! /bin/bash
 
-LATEST=$(curl --header "Accept: application/vnd.github.v3+json" https://api.github.com/repos/nlesc-recruit/cudawrappers/releases/latest | jq .tag_name --raw-output)
-cd external/cudawrappers
-MY_TAGS=$(git tag --points-at HEAD)
-echo $MY_TAGS
-for tag in $MY_TAGS
-do
-    echo "Checking tag $tag..."
-    if [ "$tag" = "$LATEST" ]; then
-        echo "It's all good., this repo's submodule is using cudawrappers latest tag $tag."
-        exit 0
-    fi
-done
-echo "This repo's submodule is using an older version of the cudawrappers library."
-exit 1
+LATEST_TAG=\"$(curl --silent --header "Accept: application/vnd.github.v3+json" https://api.github.com/repos/nlesc-recruit/cudawrappers/releases/latest | jq .tag_name --raw-output)\"
+LATEST_REF=$(curl --silent --header "Accept: application/vnd.github.v3+json"   https://api.github.com/repos/nlesc-recruit/cudawrappers/tags | jq -c ".[] | select( .name | contains($LATEST_TAG))".commit.sha --raw-output)
+MY_REF=$(git submodule status external/cudawrappers/ |  awk '{print $1}')
+
+if [ "$MY_REF" = "$LATEST_REF" ]; then
+    echo "It's all good, this repo's submodule is using the most recent version of the cudawrappers library."
+    exit 0
+else
+    echo "This repo's submodule is using an older version of the cudawrappers library."
+    exit 1
+fi
